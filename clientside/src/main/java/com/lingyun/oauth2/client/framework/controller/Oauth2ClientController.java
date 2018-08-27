8f563180316f0e06098cda9999bb35bab5006f4c
xulei <862912218@qq.com>
2018-08-27 12:22:24 +0800

    000

--- clientside/src/main/java/com/lingyun/oauth2/client/framework/controller/Oauth2ClientController.java
+++ clientside/src/main/java/com/lingyun/oauth2/client/framework/controller/Oauth2ClientController.java
@@ -3,10 +3,17 @@ package com.lingyun.oauth2.client.framework.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class Oauth2ClientController {
     @RequestMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request, HttpServletResponse response){
+        response.setStatus(401);
+        System.out.println(request.getAuthType());
+        response.setHeader("WWW-Authenticate","sfsdfdsfdsfdsfdsfdsds");
+        response.setHeader("sdfsd-sfsfdsfds","sfsdfdsfdsfdsfdsfdsds");
         return "home";
     }
 }
